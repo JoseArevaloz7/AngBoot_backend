c0ca8d8ae02f7b0952f538d993107f794b34b473
JoseArevaloz7 <arevaloz_j@outlook.com>
2024-12-11 20:35:44 -0600

    Start security

---
+++ src/main/java/com/jg/book/user/User.java
@@ -0,0 +1,9 @@
+package com.jg.book.user;
+
+import java.security.Principal;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails, Principal{
+
+}
