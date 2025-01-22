72109d8bc0781dfb09e1a4c06b0afc6ce65338c6
JoseArevaloz7 <arevaloz_j@outlook.com>
2025-01-21 20:30:52 -0600

    + generate token

--- src/main/java/com/jg/book/security/JwtFilter.java
+++ src/main/java/com/jg/book/security/JwtFilter.java
@@ -11,13 +11,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull  FilterChain filterChain ) throws ServletException, IOException {
         if(request.getServletPath().contains("api/v1/auth")){
+            filterChain.doFilter(request, response);
             return;
         }
         final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
@@ -25,8 +30,11 @@ public class JwtFilter extends OncePerRequestFilter {
         final String userEmail;
 
         if(authHeader == null || !authHeader.startsWith("Bearer")){
-            
+            filterChain.doFilter(request, response);
+            return;
         }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 
 }
---
+++ src/main/java/com/jg/book/security/JwtService.java
@@ -0,0 +1,69 @@
+package com.jg.book.security;
+
+import java.lang.foreign.ValueLayout;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+        
+    private String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return buildToken(claims, userDetails, jwtExpiration);
+    }
+        
+    private String buildToken(Map<String, Object> claims, UserDetails userDetails, long jwtExpiration) {
+        var authorities = userDetails.getAuthorities()
+            .stream()
+            .map(GrantedAuthority::getAuthority)
+            .toList();
+        
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .claim("authorities", authorities)
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userName = extractUsername(token);
+    }
+        
+    public String extractUsername(String token) {
+        return extractClamin(token, Claims::getSubject);
+    }
+                
+    private <T> T extractClamin(String token, Function<Claims, T> claimsResolver) {
+        final claims = extractAllClaims(token);
+    }
+        
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
